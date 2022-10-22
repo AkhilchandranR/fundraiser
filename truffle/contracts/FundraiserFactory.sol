539a9a8e9fedb15e5980bb4e3396facc6450dd38
AkhilchandranR <akhilchandran771@gmail.com>
2022-10-22 14:06:48 +0530

    fudraiserfactory completed/offset, boundary conditions tested and passed

--- truffle/contracts/FundraiserFactory.sol
+++ truffle/contracts/FundraiserFactory.sol
@@ -7,6 +7,8 @@ contract FundraiserFactory{
 
     Fundraiser[] private _fundraisers;
 
+    uint256 constant maxLimit = 20;
+
     event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
 
     function fundraisersCount() public view returns(uint256){
@@ -36,6 +38,15 @@ contract FundraiserFactory{
     }
 
     function fundraisers(uint256 limit, uint256 offset) public view returns(Fundraiser[] memory coll){
+        require(offset <= fundraisersCount(), "offset out of bounds");
+
+        uint256 size = fundraisersCount() < limit ? fundraisersCount() : limit;
+        size = size < maxLimit ? size : maxLimit;
+        coll = new Fundraiser[](size);
+
+        for(uint256 i = 0; i< size; i++){
+            coll[i] = _fundraisers[offset + i];
+        }
 
         return coll;
     }
--- truffle/test/fundraiser_factory_test.js
+++ truffle/test/fundraiser_factory_test.js
@@ -1,4 +1,5 @@
 const FundraiserFactoryContract = artifacts.require("FundraiserFactory");
+const FundraiserContract = artifacts.require("Fundraiser");
 
 contract("FundraiserFactory: deployment",()=>{
     it("FundraiserFactory has been deployed", async()=>{
@@ -85,15 +86,67 @@ contract("Get fundraisers",(accounts)=>{
             assert.equal(fundraisers.length,10,"results size should be 10");
         });
 
-        xit("returns 20 when limit is 20", async()=>{
+        it("returns 20 when limit is 20", async()=>{
             const fundraisers = await factory.fundraisers(20, 0);
             assert.equal(fundraisers.length, 20, "result size should be 20");
         })
 
-        xit("returns 20 results when limit requested is 30", async ()=>{
+        it("returns 20 results when limit requested is 30", async ()=>{
             const fundraisers = await factory.fundraisers(30, 0);
             assert.equal(fundraisers.length,20,"results size should not exceed 20");
         });
         
     })
+
+    describe("varying offset", () => {
+        let factory;
+        beforeEach(async () => {
+            factory = await createFundraiserFactory(10, accounts);
+        });
+
+        it("contains the fundraiser with the appropriate offset", async ()=>{
+            const fundraisers = await factory.fundraisers(1, 0);
+            const fundraiser = await FundraiserContract.at(fundraisers[0]);
+            const name = await fundraiser.name();
+            assert.ok(await name.includes(0), `${name} did not include the offset`);
+        });
+
+        it("contains the fundraiser with the appropriate offset", async ()=>{
+            const fundraisers = await factory.fundraisers(1, 7);
+            const fundraiser = await FundraiserContract.at(fundraisers[0]);
+            const name = await fundraiser.name();
+            assert.ok(await name.includes(7), `${name} did not include the offset`);
+        });
+    });
+
+    describe("boundary conditions", () => {
+        let factory;
+        beforeEach(async () => {
+            factory = await createFundraiserFactory(10, accounts);
+        });
+
+        it("raises out of bounds error", async () => {
+            try {
+                await factory.fundraisers(1, 11);
+                assert.fail("error was not raised");
+            } catch(err) {
+                const expected = "offset out of bounds";
+                assert.ok(err.message.includes(expected), `${err.message}`);
+            }
+        });
+
+        xit("adjusts return size to prevent out of bounds error", async () => {
+            try {
+                const fundraisers = await factory.fundraisers(10, 5);
+                assert.equal(
+                fundraisers.length,
+                5,
+                "collection adjusted"
+                );
+            } catch(err) {
+                assert.fail("limit and offset exceeded bounds");
+            }
+        });
+    });
+
 });
